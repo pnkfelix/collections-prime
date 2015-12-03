76ed8954411f11e0b92b2e5452029c77ddbe4d5f
Felix S. Klock II <pnkfelix@pnkfx.org>
2015-12-03 16:03:49 +0100

    features and imports to get `cargo build && cargo test` going.
    (note this is still only testing the import of `std::collections` content.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
 #![feature(unsafe_no_drop_flag)]
 #![feature(dropck_parametricity)]
+#![feature(filling_drop)]
+#![feature(collections_bound, heap_api, wrapping, unique, oom)]
+#![feature(core, alloc, collections)]
+
+#![cfg_attr(test, feature(test, range_inclusive))]
 
 #[path="std/mod.rs"]
 mod std_mod;
@@ -13,6 +18,9 @@ pub use std::{borrow, clone, cmp, default, fmt, hash, iter};
 pub use std::{marker, mem, num, ops, option, ptr, result};
 
 pub use std_mod::collections as collections;
+
+#[cfg(test)]
+pub use std::{cell};
 
 #[test]
 fn it_works() {
